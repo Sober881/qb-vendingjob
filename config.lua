cabc11503793e4f12a0e1a2847fdc8573af5ce6b
Sober <90927749+Sober881@users.noreply.github.com>
2023-12-14 02:10:51 +0200

    add

---
+++ config.lua
@@ -0,0 +1,18 @@
+Config ={}
+
+Config.VendingJob = {
+    StartJob = vector3(766.75, -1317.62, 26.28), -- Start Work
+    VehicleSpawn = vector4(773.39, -1328.55, 26.24, 272.04), -- Spawn Van
+    Locations = { -- Job Locations
+        [1] = vector4(461.04, -705.5, 27.37, 271.48),
+        [2] = vector4(-333.22, -783.49, 33.96, 226.29),
+    }
+}
+
+Config.Notify = {
+    govan = 'КАЧИ ВЪВ ВАНА И ОТИДИ ДО СЛЕДВАЩАТА ЛОКАЦИЯ',
+    loadmachine = 'МАШИНАТА Е ЗАРЕДЕНА',
+    startjob = 'ЗАПОЧНА РАБОТА',
+    stopjob = 'СПРЯ ТЕКУЩАТА РАБОТА',
+    BLIPNAME = 'Доставки',
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+shared_script {
+    'config.lua'
+}
+
+server_scripts {
+    'server/server.lua'
+}
+client_scripts{
+    'client/client.lua'
+}
